12079314116bd35cf8c007dff9c670a9e6034835
chunildo44 <chunildo44@gmail.com>
2026-02-05 11:47:56 +0900

    [추가] 다른 예시 추가

--- section01/chapter01/chapter28.js
+++ section01/chapter01/chapter28.js
@@ -1,3 +1,4 @@
+/*
 console.log(1);
 //비동기로 처리한 방식(Web APIs 에서 실행된다)
 setTimeout(() => {
@@ -5,5 +6,17 @@ setTimeout(() => {
 }, 3000);
 
 console.log(3);
+*/
+
+// 함수 선언식
+function task(a, b) {
+  setTimeout(() => {
+    let sum = a + b;
+    console.log(`a + b = ${sum}`);
+  }, 3000);
+}
+
+// 함수 호출
+task(10, 20);
 
 
